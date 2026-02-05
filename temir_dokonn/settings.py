0c162037588d15b9fa50a9aac9a27f74f30098d8
EditBackend <commanderx002@gmail.com>
2026-02-05 15:51:51 +0500

    Fix  settings

--- temir_dokonn/settings.py
+++ temir_dokonn/settings.py
@@ -22,7 +22,7 @@ SECRET_KEY = 'django-insecure-6#5&%g7@q(-&81^ehb1&m*+x9$+q0@%0+7r6#0b4ad(=zpm_ap
 DEBUG = True
 
 ALLOWED_HOSTS = [
-    "temir-dokonn.onrender.com",
+    "temir-dokonn-1.onrender.com",
     "localhost",
     "127.0.0.1",
 ]
@@ -82,11 +82,11 @@ WSGI_APPLICATION = 'temir_dokonn.wsgi.application'
 CORS_ALLOW_ALL_ORIGINS = True
 
 CORS_ALLOWED_ORIGINS = [
-    "https://temir-dokonn.onrender.com",
+    "https://temir-dokonn-1.onrender.com",
 ]
 
 CSRF_TRUSTED_ORIGINS = [
-    "https://temir-dokonn.onrender.com",
+    "https://temir-dokonn-1.onrender.com",
 ]
 
 
