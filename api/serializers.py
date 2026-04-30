e4f8f8458a392042192d207c32347bafde3fb41b
EditBackend <commanderx002@gmail.com>
2026-04-30 14:48:07 +0500

    Diyorb3ck supplier yangiliklar

--- api/serializers.py
+++ api/serializers.py
@@ -43,12 +43,10 @@ User = get_user_model()
     #     return instance
 
 
-
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
         model = Role
         fields = "__all__"
-
 
 class EmployeeSerializer(serializers.ModelSerializer):
     class Meta:
@@ -59,76 +57,44 @@ class CustomerSerializer(serializers.ModelSerializer):
     class Meta:
         model = Customer
         fields = [
-            "id",
-            "first_name",
-            "last_name",
-            "phone",
-            "address",
-            "debt",
-            "total_debt",
-            "created_at",
+            "id", "first_name", "last_name", "phone",
+            "address", "debt", "total_debt", "created_at",
         ]
-
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = "__all__"
 
-
-# class PaymentTypeSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = PaymentType
-#         fields = "__all__"
-
-
-class ProductSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Product
-        fields = '__all__'
-
-
-
-class SaleSerializer(serializers.ModelSerializer):
-
-    product_name = serializers.CharField(
-        source='product.name',
-        read_only=True
-    )
-
-    payment_type_name = serializers.CharField(
-        source='payment_type.name',
-        read_only=True
-    )
-
-    class Meta:
-        model = Sale
-        fields = '__all__'
-        read_only_fields = (
-            'total_price',
-            'created_at',
-            'check_number'
-        )
-
-
 class SupplierSerializer(serializers.ModelSerializer):
     class Meta:
         model = Supplier
         fields = "__all__"
 
+class ProductSerializer(serializers.ModelSerializer):
+    # Agar Product modelida supplier degan ForeignKey bo'lsa, uning nomini chiqarish:
+    supplier_name = serializers.CharField(source='supplier.name', read_only=True)
+    category_name = serializers.CharField(source='category.name', read_only=True)
 
+    class Meta:
+        model = Product
+        fields = '__all__' # Bu barcha fieldlarni, plyus yuqoridagi 'supplier_name'ni ham chiqaradi
+
+class SaleSerializer(serializers.ModelSerializer):
+    product_name = serializers.CharField(source='product.name', read_only=True)
+    payment_type_name = serializers.CharField(source='payment_type.name', read_only=True)
+
+    class Meta:
+        model = Sale
+        fields = '__all__'
+        read_only_fields = ('total_price', 'created_at', 'check_number')
 
 class WarehouseIncomeSerializer(serializers.ModelSerializer):
     class Meta:
         model = WarehouseIncome
         fields = "__all__"
-        read_only_fields = (
-            'created_at',
-            'check_number'
-        )
+        read_only_fields = ('created_at', 'check_number')
 
-
-#ombor kirimi partiya
 class BatchSerializer(serializers.ModelSerializer):
     product_name = serializers.CharField(source='product.name', read_only=True)
     supplier_name = serializers.CharField(source='supplier.name', read_only=True)
@@ -143,7 +109,6 @@ class ExpenseCategorySerializer(serializers.ModelSerializer):
         model = ExpenseCategory
         fields = ['id', 'name']
 
-
 class ExpenseSerializer(serializers.ModelSerializer):
     category = ExpenseCategorySerializer(read_only=True)
 
@@ -151,11 +116,10 @@ class ExpenseSerializer(serializers.ModelSerializer):
         model = Expense
         fields = '__all__'
 
-
 class ExpenseCreateSerializer(serializers.ModelSerializer):
     class Meta:
         model = Expense
-        fields = ['date', 'category', 'amount', 'payment_type', 'note',]
+        fields = ['date', 'category', 'amount', 'payment_type', 'note', 'branch']
 
     def validate_amount(self, value):
         if value <= 0:
@@ -165,20 +129,3 @@ class ExpenseCreateSerializer(serializers.ModelSerializer):
     def create(self, validated_data):
         validated_data['created_by'] = self.context['request'].user
         return super().create(validated_data)
-
-    from rest_framework import serializers
-    from .models import Expense
-
-    class ExpenseCreateSerializer(serializers.ModelSerializer):
-        class Meta:
-            model = Expense
-            fields = ['date', 'category', 'amount', 'payment_type', 'note', 'branch']
-
-        def validate_amount(self, value):
-            if value <= 0:
-                raise serializers.ValidationError("Amount musbat bo‘lishi kerak")
-            return value
-
-        def create(self, validated_data):
-            validated_data['created_by'] = self.context['request'].user
-            return super().create(validated_data)
