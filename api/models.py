7441e67132fe33cba49aa434f073cbc4debcdf38
EditBackend <commanderx002@gmail.com>
2026-05-23 15:24:28 +0500

    eng so'nggi hatolarni tuzatdim Diyorb3ck

--- api/models.py
+++ api/models.py
@@ -126,7 +126,7 @@ class Customer(models.Model):
     address = models.CharField(max_length=255, blank=True, null=True)
 
     debt = models.DecimalField(max_digits=14, decimal_places=2, default=0)
-    total_debt = models.DecimalField(max_digits=14, decimal_places=2, default=0)
+    total_debt = models.DecimalField(max_digits=20, decimal_places=2, default=0.00)
     score = models.IntegerField(default=10)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)  # Har safar save() bo'lganda yangilanadi
