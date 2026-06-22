9cb2d690f59def09927deb19b56a4db2cf1f37f8
EditBackend <commanderx002@gmail.com>
2026-06-22 16:57:33 +0500

    InshaAlloh

--- temir_dokonn/urls.py
+++ temir_dokonn/urls.py
@@ -4,19 +4,20 @@ from django.conf import settings
 from django.conf.urls.static import static
 from api.views import home
 
-
 urlpatterns = [
+    # 1. Admin panel
     path('admin/', admin.site.urls),
+
+    # 2. Avtorizatsiya va tashkilot yo'llari (Buni tepaga olib chiqamiz)
+    # Yakuniy url: api/auth/forget-password/ va hokazo bo'ladi
+    path('api/', include('organization.urls')),
+
+    # 3. Qolgan barcha umumiy API pichoqlari (CRUD, ViewSet'lar)
     path('api/', include('api.urls')),
-    path('api/', include('organization.urls')),
 
-
-    # path("api/", include("ai.urls")),
+    # 4. Bosh sahifa
     path('', home),
-    path('api/', include('organization.urls')),
 ]
-
-
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
